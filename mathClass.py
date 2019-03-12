3a773b8dff24d86729edfdae82f4fd27649754ad
Kim Fairbanks <jdfairbanks@alaska.edu>
2019-03-12 15:08:50 -0800

    1D post-collision detection working. 2D pending

--- mathClass.py
+++ mathClass.py
@@ -45,12 +45,48 @@ class Collider:
     def __init__(self, sprite, isStatic, position):
         self.g = 9.8                #gravity
         self.isStatic = isStatic
-        self.position = Vec2D(0, 0)
+        self.position = Vec2D(position.getX(), position.getY())
+        self.width = sprite[0]
+        self.height = sprite[1]
+
+    def getWidth(self):
+        return self.width
+    
+    def getHeight(self):
+        return self.height
 
     def update(self):
         self.physics()
 
     def physics(self):
         otherVec = Vec2D(2, 2)
-
         self.position.addVec(otherVec)
+
+
+##########################################################
+#              W O R L D  S P A C E                      #
+#                   C L A S S                            #
+##########################################################
+
+class worldSpace:
+    def __init__(self, gravity):
+        self.g = gravity
+
+    def update(self, colliderList):
+        for obj in colliderList:
+            if obj.isStatic == True:    #If the collider is from an enviroment obj, we ignore it
+                for otherObj in colliderList:   #checks every gameobject's collider against the other
+
+
+                #CHECK LEFT SIDE of OBJ vs RIGHT SIDE of other.
+                    if (obj.position.getX() <= otherObj.position.getX() + otherObj.getWidth()
+                    and obj.position.getX() > otherObj.position.getX()):                        
+                        offset = (otherObj.position.getX() + otherObj.getWidth()) - obj.position.getX() + 1
+                        obj.position.setX(obj.position.getX() + offset)
+                
+                    #CHECK BOTTOM of OBJ vs TOP of other.
+                    if (obj.position.getY() <= otherObj.position.getY() + otherObj.getHeight()
+                    and obj.position.getY() > otherObj.position.getY()):                        
+                        offset = (otherObj.position.getX() + otherObj.getWidth()) - obj.position.getX() + 1
+                        obj.position.setX(obj.position.getX() + offset)
+                
--- mathTest.py
+++ mathTest.py
@@ -8,10 +8,38 @@ velocity00.addVec(velocity01)
 # print(velocity00.getX())
 
 
-playerPosition = mathClass.Vec2D(5, 5)
+playerPosition = mathClass.Vec2D(14, 14)
 playerCollider = mathClass.Collider((2, 2), False, playerPosition)
 
 
-for x in range (1, 5):
-    playerCollider.update()
-    print(playerCollider.position.getX())
+# for x in range (1, 5):
+#     playerCollider.update()
+#     print(playerCollider.position.getX())
+
+boxTestPosition = mathClass.Vec2D(15, 15)
+boxTestCollider = mathClass.Collider((5,5), True, boxTestPosition)
+
+
+
+colliderList = []
+colliderList.append(playerCollider)
+colliderList.append(boxTestCollider)
+
+world = mathClass.worldSpace(9.8)
+
+
+print("PLAYER X PRIOR POS \n")
+print(playerCollider.position.getX())
+print("\n")
+    
+print("BOX X PRIOR POS \n")
+print(boxTestCollider.position.getX())
+
+
+world.update(colliderList)
+print("\n")
+print("PLAYER X END POS \n")
+print(playerCollider.position.getX())
+print("\n")
+print("BOX X POS")
+print(boxTestCollider.position.getX())
